001a0b67e9f155cfcee74d9862d38c568887abce
Michal Mirus <mirusmichal@gmail.com>
2026-05-01 16:47:43 +0200

    feat: save tasks to json

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-print("Welcome to task-tracker")
+import os
+import json
 
-tasks = []
+# objectives to accomplish:
+# load contents of the task list from the file if the file exists
+# if it doesn't - create new file for that purpose
+# allow to append tasks to the end of the list
 
-def add_task(task_name):
+TASKS_FILE_NAME = "tasks.json"
+
+def load_tasks():
+    if not os.path.exists(TASKS_FILE_NAME):
+        return []
+    
+    with open(TASKS_FILE_NAME, "r", encoding="utf-8") as f:
+        return json.load(f)
+    
+def save_tasks(tasks):
+    with open(TASKS_FILE_NAME, "w", encoding="utf-8") as f:
+        json.dump(tasks, f, indent=4, ensure_ascii=False)
+
+
+def add_task(tasks, task_name):
     tasks.append({"name": task_name, "done": False})
+    save_tasks(tasks)
     print(f"dodano zadanie {task_name}")
 
-add_task("Learn git!")
-
+tasks = load_tasks()
+add_task(tasks, "Nauczyć się gita i jsona!")
 print("zadania: ", tasks)
-
+    
