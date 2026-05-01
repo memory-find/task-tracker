aaa9af2200f33eed04ae2cf69e4491a8ec4bc1cf
Michal Mirus <mirusmichal@gmail.com>
2026-05-01 16:11:49 +0200

    feat: add task function

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 print("Welcome to task-tracker")
+
+tasks = []
+
+def add_task(task_name):
+    tasks.append({"name": task_name, "done": False})
+    print(f"dodano zadanie {task_name}")
+
+add_task("Learn git!")
+
+print("zadania: ", tasks)
+
