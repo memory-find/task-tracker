5f159babf1a7de5e6041ce2e32541e59ce4a6cfc
Michal Mirus <mirusmichal@gmail.com>
2026-05-22 09:38:32 +0200

    feat: data format unification/priority implement

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ import sys
 FILE_NAME = "tasks.json"
 
 def menu():
+    loaded_tasks_for_conversion = load_tasks_list()
+    loaded_tasks_after_conversion = data_migration(loaded_tasks_for_conversion)
+    save_task_file(loaded_tasks_after_conversion)
+
     while True:
-        decision = input("choose one of options:\n 1. List tasks\n 2. Filter tasks by status\n 3. Add new task\n 4. Change status\n 5. Remove task.\n 6. Edit task descrpiton\n 7. Close")
+        decision = input("choose one of options:\n 1. List tasks\n 2. Filter tasks by status or priority\n 3. Add new task\n 4. Change status\n 5. Remove task.\n 6. Edit task descrpiton\n 7. Change priority\n 8. Close")
         loaded_tasks = load_tasks_list()
 
 
@@ -16,11 +20,11 @@ def menu():
 
         
         elif decision == "2":
-            filter_tasks_by_status(loaded_tasks)
+            filter_tasks_by_status_or_prio(loaded_tasks)
            
 
         elif decision == "3":
-            new_task = input("what would you like to add?")
+            new_task = input("what would you like to add?").strip()
             write_tasks(loaded_tasks, new_task)
 
 
@@ -36,6 +40,10 @@ def menu():
 
 
         elif decision == "7":
+            change_priority(loaded_tasks)
+
+
+        elif decision == "8":
             sys.exit()
 
 
@@ -43,7 +51,25 @@ def menu():
             print("No such option, try again")
 
 
+# data migration function to be run once before menu loop is initiated, while initiated, isDone key is being replaced by status
+# legacy tasks without priority will have medium priority assigned by default
+def data_migration(loaded_tasks):
+    
+    for record in loaded_tasks:
 
+        if record.get('status') is None:
+            if record['isDone'] == "True":
+                record.pop('isDone')
+                record['status'] = "done"
+            
+            elif record['isDone'] == "False":
+                record.pop('isDone')
+                record['status'] = "to-do"
+
+        if record.get('priority') is None:
+            record['priority'] = "medium"
+
+    return loaded_tasks
 
 
 # check wheter the file exists, if not - load empty list
@@ -57,26 +83,64 @@ def load_tasks_list():
 
 # introducing new feature for listing tasks by its status
 # function can serve also legacy capitalized 'Done' status
-def filter_tasks_by_status(loaded_tasks):
+def filter_tasks_by_status_or_prio(loaded_tasks):
+    
     try:
-            decision = int(input("\nWhich status would you like to filter: \n1. To-do\n2. In-progress\n3. Done"))
+    
+        status_or_prio = int(input("\nWhich filtering mechanism would you like to apply?: \n1. Status\n2. Priority"))    
+
+        if status_or_prio == 1:
+
+            try:
+                    decision_status = int(input("\nWhich status would you like to filter: \n1. To-do\n2. In-progress\n3. Done"))
+                            
+                    if decision_status == 1:
+                        filter_tasks_by_status_local(loaded_tasks, "to-do")
+
+                    elif decision_status == 2:
+                        filter_tasks_by_status_local(loaded_tasks, "in-progress")
+
+                    elif decision_status == 3:
+                        filter_tasks_by_status_local(loaded_tasks, "done")
+
+                    else:
+                        print("\n-----------START-----------")
+                        print("Incorrect option, try again")
+                        print("-----------END-----------\n")
                     
-            if decision == 1:
-                filter_tasks_by_status_local(loaded_tasks, "to-do")
+            except ValueError:
+                print("\nIncorrect type, type an integer\n")
 
-            elif decision == 2:
-               filter_tasks_by_status_local(loaded_tasks, "in-progress")
 
-            elif decision == 3:
-                filter_tasks_by_status_local(loaded_tasks, "done")
-
-            else:
-                print("\n-----------START-----------")
-                print("Incorrect option, try again")
-                print("-----------END-----------\n")
+        elif status_or_prio == 2:
             
+            try:
+                    decision_prio = int(input("\nWhich priority would you like to filter: \n1. low\n2. medium\n3. high"))
+                            
+                    if decision_prio == 1:
+                        filter_tasks_by_priority_local(loaded_tasks, "low")
+
+                    elif decision_prio == 2:
+                        filter_tasks_by_priority_local(loaded_tasks, "medium")
+
+                    elif decision_prio == 3:
+                        filter_tasks_by_priority_local(loaded_tasks, "high")
+
+                    else:
+                        print("\n-----------START-----------")
+                        print("Incorrect option, try again")
+                        print("-----------END-----------\n")
+                    
+            except ValueError:
+                print("\nIncorrect type, type an integer\n")
+
+        else:
+            print("\n-----------START-----------")
+            print("Incorrect option, try again")
+            print("-----------END-----------\n") 
+
     except ValueError:
-        print("\nIncorrect type, type an integer\n")
+            print("\nIncorrect type, type an integer\n")
 
 
 #unifing filtering operations, and making one general function for it
@@ -84,14 +148,20 @@ def filter_tasks_by_status_local(loaded_tasks, task_status):
     print("\n-----------START-----------")
     for index, task in enumerate(loaded_tasks, start=1):
         
-        if task.get('status') is None:
-            continue
-
         if (task["status"]).lower() == task_status:
             print(f"{index}. {(task['task']).capitalize()} | status: {task['status']}")
     print("-----------END-----------\n")
 
 
+
+#unifiyng filtering by priority
+def filter_tasks_by_priority_local(loaded_tasks, task_priority):
+    print("\n-----------START-----------")
+    for index, task in enumerate(loaded_tasks, start=1):
+        
+        if (task["priority"]).lower() == task_priority:
+            print(f"{index}. {(task['task']).capitalize()} | status: {task['status']} | priority: {task['priority']}")
+    print("-----------END-----------\n")
 
 
 # showing tasks in order
@@ -101,27 +171,20 @@ def show_tasks(loaded_tasks):
         print("Woho! Nothing to be done for now...")
         return
 
-    try:
+    else:
         print("\n-----------START-----------")
         for index, task in enumerate(loaded_tasks, start=1):
 
-            if task.get('status') is None:
-                print(f"{index}. {(task['task']).capitalize()} | isDone: {task['isDone']}")
-            
-            else:
-                print(f"{index}. {(task['task']).capitalize()} | status: {task['status']}")
-        
+            print(f"{index}. {(task['task']).capitalize()} | status: {task['status']} | priority: {task['priority']}")
+
         print("-----------END-----------\n")
 
-    except KeyError:
-        print(f"legacy task: {task}")
-        print("-----------END-----------\n")
-        pass
 
 # appending new tasks at the bottom
 # instead isDone, changing the key naming to 'status' and by default setting it to to_do 
+# priority feature implementation, new tasks by default will land with new key: 'priority' and it will be set to medium
 def write_tasks(file_from_load_tsk, new_task):
-    file_from_load_tsk.append({"task": new_task, "status": "to-do"})
+    file_from_load_tsk.append({"task": new_task, "status": "to-do", "priority": "medium"})
     save_task_file(file_from_load_tsk)
     print(f"task added: {new_task}")
 
@@ -208,16 +271,10 @@ def set_the_status(loaded_tasks):
 
 # setting one general function to run inside set_the_status for each condition, allowing to avoid repeatitions in code
 def set_the_status_local(loaded_tasks, index_for_completion, status_to_be_set):
-    if loaded_tasks[ index_for_completion -1 ].get("status") is None:
-        loaded_tasks[ index_for_completion -1 ].pop('isDone')
-        loaded_tasks[ index_for_completion -1 ]["status"] = status_to_be_set
-        print(f"\nStatus set as '{status_to_be_set}' for: ({loaded_tasks[index_for_completion -1]["task"]})\n")
-        save_task_file(loaded_tasks)
-        
-    else:
-        loaded_tasks[ index_for_completion -1 ]["status"] = status_to_be_set
-        print(f"\nStatus set as '{status_to_be_set}' for: ({loaded_tasks[index_for_completion -1]["task"]})\n")
-        save_task_file(loaded_tasks)
+   
+    loaded_tasks[ index_for_completion -1 ]["status"] = status_to_be_set
+    print(f"\nStatus set as '{status_to_be_set}' for: ({loaded_tasks[index_for_completion -1]["task"]})\n")
+    save_task_file(loaded_tasks)
 
 
 # edit description for the already added task
@@ -244,7 +301,7 @@ def edit_task_description(loaded_tasks):
                     print("You cannot leave empty space as description\n")
                 
                 else:
-                    loaded_tasks[index_for_changing_status -1]["task"] = new_description
+                    loaded_tasks[index_for_changing_status -1]["task"] = new_description.strip()
                     save_task_file(loaded_tasks)
                     print("Description has been changed!")
                     break
@@ -253,6 +310,54 @@ def edit_task_description(loaded_tasks):
         print("\nIncorrect type, type integer\n")
 
 
+def change_priority(loaded_tasks):
+
+    if loaded_tasks == []:
+        print("Woho! Nothing to change priority for...")
+        return
+
+    
+    show_tasks(loaded_tasks)
+    
+
+    try: 
+        index_for_prio = int(input("Choose task for changing priority"))
+        
+        if index_for_prio <= 0 or index_for_prio > len(loaded_tasks):
+            print(f"\nMake sure that integer chosen is bigger than 0 and it fits in current amount of tasks: {len(loaded_tasks)}\n")
+
+        else:
+            try:
+                priority = int(input("\n1. low\n2. medium\n3. high"))
+
+                
+                if priority == 1:
+                    set_the_prio_local(loaded_tasks, index_for_prio, "low")
+
+                elif priority == 2:
+                    set_the_prio_local(loaded_tasks, index_for_prio, "medium")
+
+                elif priority == 3:
+                    set_the_prio_local(loaded_tasks, index_for_prio, "high")
+
+                else: 
+                    print("\nOnly options 1, 2 and 3 are valid. Try again\n")
+            
+            except ValueError:
+                print("\nIncorrect type, type integer\n")
+    
+    except ValueError:
+        print("\nIncorrect type, type integer\n")
+
+
+
+# setting one general function to run inside change_priority for each condition, allowing to avoid repeatitions in code
+def set_the_prio_local(loaded_tasks, index_for_prio, prio_to_be_set):
+    
+    loaded_tasks[ index_for_prio -1 ]["priority"] = prio_to_be_set
+    print(f"\nPriority set as '{prio_to_be_set}' for: ({loaded_tasks[index_for_prio -1]["task"]})\n")
+    save_task_file(loaded_tasks)
+        
 
 
 if __name__ == "__main__":
